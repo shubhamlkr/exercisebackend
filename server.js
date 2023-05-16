a437dbd144e49aded1adb76416bc1d89d77c4c27
shubhamlkr <shubhamkr78700@gmail.com>
2023-05-16 11:57:36 +0530

    c

---
+++ server.js
@@ -0,0 +1,28 @@
+require('dotenv').config()
+const express= require('express')
+const workoutroutes=require('./routes/workouts')
+const mongoose=require('mongoose')
+
+const cors = require('cors');
+
+const app=express()
+
+app.use(express.json())
+app.use(cors());
+app.use((req,res,next)=>{
+    console.log(req.path,req.method);
+    next();
+})
+
+app.use('/api/workouts',workoutroutes)
+
+mongoose.connect(process.env.MONG_URI)
+.then(()=>{
+    app.listen(process.env.PORT,()=>{
+        console.log('connected to db and listening on port',process.env.PORT,'!!') 
+    })
+})
+.catch((error)=>{
+    console.log(error);
+})
+
